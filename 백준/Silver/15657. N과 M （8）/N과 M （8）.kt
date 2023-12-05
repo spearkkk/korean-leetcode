9fcf1cb7b0d8e23e7cdeda6066d31ed963237d2d
ChangKwon Jeong <spearkkp@gmail.com>
2023-12-05 22:10:46 +0900

    [Silver III] Title: N과 M (8), Time: 244 ms, Memory: 31604 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.kt
@@ -0,0 +1,32 @@
+fun main() {
+    val (n, m) = readInts()
+    val numbers = readInts().sorted()
+
+    val answer = IntArray(m) // 선택한 수를 저장하는 배열
+
+    val stringBuilder = StringBuilder()
+
+    fun bt(count: Int, baseIdx: Int) {
+        if (count == m) {
+            stringBuilder.append(answer.joinToString(" ")) // 결과를 바로 출력하지 않고 스트링 빌더를 이용한다.
+            stringBuilder.append("\n")
+            return
+        }
+
+        for (idx in baseIdx until n) {
+            val tmp = numbers[idx]
+
+            answer[count] = tmp
+            bt(count + 1, idx)
+        }
+
+    }
+
+    bt(0, 0)
+    println(stringBuilder)
+}
+
+fun readStr() = readln()
+fun readInt() = readStr().toInt()
+fun readStrings() = readStr().split(" ")
+fun readInts() = readStrings().map { it.toInt() }
