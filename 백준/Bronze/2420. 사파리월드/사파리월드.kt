5dd40c8f49ca862b5e45304c0e980338b289fe00
ChangKwon Jeong <spearkkp@gmail.com>
2023-12-11 00:16:34 +0900

    [Bronze V] Title: 사파리월드, Time: 140 ms, Memory: 20928 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val (n, m) = readLongs()
+    println(kotlin.math.abs(n - m))
+}
+
+fun readStr() = readln()
+fun readStrings() = readStr().split(" ")
+fun readInt() = readStr().toInt()
+fun readLong() = readStr().toLong()
+fun readInts() = readStrings().map { it.toInt() }
+fun readLongs() = readStrings().map { it.toLong() }
