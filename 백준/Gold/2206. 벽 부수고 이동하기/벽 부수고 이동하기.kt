1faa834019cdb844d8104462463552d19fe97603
ChangKwon Jeong <spearkkp@gmail.com>
2024-01-14 22:20:07 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 2064 ms, Memory: 346192 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.kt
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.kt
@@ -1,4 +1,5 @@
 fun main() {
+    // ref.) https://kscodebase.tistory.com/66
     val dirY = intArrayOf(1, 0, -1, 0)
     val dirX = intArrayOf(0, 1, 0, -1)
 
@@ -12,13 +13,16 @@ fun main() {
         }
     }
 
+    // visited가 2차원 배열이 아니라 3차원 배열이다. 마지막 3차원에는 벽을 부순 이력을 저장한다.
+    // visited[y][x][0 or 1]: (y, x) 위치에서 0은 벽을 부순적이 없는 상황, 1은 벽을 부순적이 있는 상황
+    // 한번도 벽을 부수지 않을 수 있으나, 이미 한번 부순 이력이 있다면 해당 이력을 계속 가져가야한다.
     val visited = Array(n) { Array(m) { BooleanArray(2) } }
 
     // (y, x) -> hasBroken
-    val queue = java.util.LinkedList<Pair<Pair<Int, Int>, Boolean>>()
+    val queue = java.util.LinkedList<Pair<Pair<Int, Int>, Boolean>>() // 첫번째 원소를 자르는 비용이 많이 발생하지 않기 위해 LinkedList
     queue.add(Pair(Pair(0, 0), false))
 
-    val anotherQueue = mutableSetOf<Pair<Pair<Int, Int>, Boolean>>()
+    val anotherQueue = mutableSetOf<Pair<Pair<Int, Int>, Boolean>>() // 다음 스테이지 사용할 위치들인데, 중복을 방지하기 위해 set
 
     var flg = false
     var answer = 1
