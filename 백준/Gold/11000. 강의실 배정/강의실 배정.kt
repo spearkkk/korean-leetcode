1f0d0891ce8a0fba92c80be39410c3066527dca7
ChangKwon Jeong <spearkkp@gmail.com>
2024-01-31 23:21:08 +0900

    [Gold V] Title: 강의실 배정, Time: 1160 ms, Memory: 137648 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.kt
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.kt
@@ -26,11 +26,19 @@ fun main() {
 
     val comparator = compareBy<APoint> { it.first }.then(compareBy<APoint> { it.second - it.first })
     points.sortWith(comparator)
-//    println(points.joinToString(" -> "))
 
+    /**
+     * 문제를 확인해보면 강의들을 겹치지 않게 배열해서 한 강의실에 듣게끔 한다.
+     * 겹치면 다른 강의실을 사용한다.
+     * 최적의로 강의를 배치했을 때, 최소로 사용하는 강의실의 수
+     * 
+     * ends는 각 강의실의 끝나는 시간을 나타낸다.
+     * 
+     */
     val ends = java.util.PriorityQueue<Int>()
 
     for (i in 0 until n) {
+        // 배치해야할 강의
         val (s, e) = points[i]
 
         if (ends.isEmpty()) {
@@ -38,8 +46,14 @@ fun main() {
             continue
         }
 
+        // 무조건 ends 에 값을 추가한다.
+        // 왜냐하면 앞서 강의를 정렬한 기준이 있기 때문에
+        // 시작 시간이 같으면 항상 ends에 있는 값보다 e의 값이 크다
+        // 시작 시간이 다르고 만약 e의 값이 ends안의 값보다 작다면, 그 두 강의 중 하나가 하나를 포함하기 때문에 두 개의 강의실을 써야한다.
+        // 시작 시간이 이미 들어간 강의 중에 제일 먼저 끝나는 시간보다 크면 그 강의실을 사용하면 된다.
         ends.add(e)
         if (ends.first() <= s) {
+            // 이미 끝난 강의실을 이어서 사용한다.
             ends.remove()
         }
     }
