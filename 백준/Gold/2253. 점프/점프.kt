cf2391f21d3d76b917c5f5d39eba1dfc0fa5651e
ChangKwon Jeong <spearkkp@gmail.com>
2025-03-19 00:53:04 +0900

    [Gold IV] Title: 점프, Time: 1548 ms, Memory: 213988 KB -BaekjoonHub

--- 백준/Gold/2253. 점프/점프.kt
+++ 백준/Gold/2253. 점프/점프.kt
@@ -1,4 +1,14 @@
 fun main() {
+    /**
+     * 최소 점프 횟수가 답이기 때문에 회수가 작은 위치를 먼저 처리한다.
+     * -> min-heap
+     * 
+     * 핵심은 위치에 도달했을 때, 위치만 고려하지 않고 속도를 같이 고려해야한다.
+     * 왜냐하면 위치와 속도에 따라서 점프할 수 있는 상태가 다르다.
+     * (위치, 속도)가 하나의 유일한 상태로 봐야한다.
+     * 배열로 이 상태를 저장하기 보다는 set으로 관리한다.
+     * 
+     */
     fun solution(n: Int, x: MutableSet<Int>): Int {
         // (count, (index, velocity))
         val pq = java.util.PriorityQueue<Pair<Int, Pair<Int, Int>>>(compareBy { it.first })
